4a3d70cbe4e8470ca7b8893856f8f9bc80a34a99
yaney <53202722+yaney01@users.noreply.github.com>
2023-02-20 10:06:20 +0800

    Update ConnectivityTest.js

--- Surge/ConnectivityTest.js
+++ Surge/ConnectivityTest.js
@@ -1,15 +1,15 @@
 let $ = {
 Bilibili:'https://www.bilibili.com',
 Baidu:'https://www.baidu.com',
-YouTube:'https://www.youtube.com/',
+Youtube:'https://www.youtube.com/',
 Google:'https://www.google.com/generate_204',
 Github:'https://www.github.com'
 }
 
 !(async () => {
-await Promise.all([http($. Baidu),http($. Bilibili),http($. Github),http($. Google),http($. YouTube)]).then((x)=>{
+await Promise.all([http('Baidu'),http('Bilibili'),http('Github'),http('Google'),http('Youtube')]).then((x)=>{
 	$done({
-    title: '网络连通性测试',
+    title: '网络连接测试',
     content: x.join('\n'),
     icon: 'timer',
     'icon-color': '#2b7bf6',
@@ -20,9 +20,9 @@ await Promise.all([http($. Baidu),http($. Bilibili),http($. Github),http($. Goog
 function http(req) {
     return new Promise((r) => {
 			let time = Date.now();
-        $httpClient.post(req, (err, resp, data) => {
-            r(req.split(".")[1]+
-						'\xa0\xa0\xa0\xa0\xa0\t: ' +
+        $httpClient.post($[req], (err, resp, data) => {
+            r(req +
+						'\xa0\xa0\xa0\t: ' +
 						(Date.now() - time)+' ms');
         });
     });
